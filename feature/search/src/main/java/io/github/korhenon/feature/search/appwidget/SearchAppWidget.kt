d7f635d56e48c328d7908230e702cac4c7cc9333
Nikolay <75427338+korhenon@users.noreply.github.com>
2025-08-26 11:29:36 +0300

    Design for search widget was realised

--- feature/search/src/main/java/io/github/korhenon/feature/search/appwidget/SearchAppWidget.kt
+++ feature/search/src/main/java/io/github/korhenon/feature/search/appwidget/SearchAppWidget.kt
@@ -2,11 +2,30 @@ package io.github.korhenon.feature.search.appwidget
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.glance.ColorFilter
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.Image
+import androidx.glance.ImageProvider
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Box
+import androidx.glance.layout.fillMaxHeight
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
+import androidx.glance.layout.padding
+import androidx.glance.unit.ColorProvider
+import io.github.korhenon.feature.search.R
 
 internal class SearchAppWidget : GlanceAppWidget() {
+    private val containerColor = Color(0xCCFFFFFF)
+    private val contentColor = Color(0xFF434359)
+    private val height = 60.dp
+
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
             Content()
@@ -15,6 +34,20 @@ internal class SearchAppWidget : GlanceAppWidget() {
 
     @Composable
     private fun Content() {
-
+        Box(
+            modifier = GlanceModifier
+                .height(height)
+                .fillMaxWidth()
+                .background(containerColor)
+                .cornerRadius(height / 2)
+                .padding(height / 10)
+        ) {
+            Image(
+                provider = ImageProvider(R.drawable.ic_search),
+                contentDescription = "Search icon",
+                colorFilter = ColorFilter.tint(ColorProvider(contentColor)),
+                modifier = GlanceModifier.fillMaxHeight()
+            )
+        }
     }
 }
