82f60f8ea0568c354cd3824144974c3504a5cfb1
Nikolay <75427338+korhenon@users.noreply.github.com>
2025-08-28 22:07:35 +0300

    Finishing app when it is not active was added

--- feature/search/src/main/java/io/github/korhenon/feature/search/screen/SearchActivity.kt
+++ feature/search/src/main/java/io/github/korhenon/feature/search/screen/SearchActivity.kt
@@ -2,15 +2,24 @@ package io.github.korhenon.feature.search.screen
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.addCallback
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
 internal class SearchActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        onBackPressedDispatcher.addCallback {
+            finishAndRemoveTask()
+        }
         enableEdgeToEdge()
         setContent {
             SearchRoot()
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        finishAndRemoveTask()
     }
 }
